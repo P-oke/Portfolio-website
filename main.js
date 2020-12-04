5124b294593459a4b19db2adc136c9d183898b37
P-oke <63716839+P-oke@users.noreply.github.com>
2020-12-04 18:07:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 const toggle = document.getElementsByClassName("toggle-bar")[0];
 const links = document.getElementsByClassName("nav-elements")[0];
-toggle.addEventListener("click", () => {
-  links.classList.toggle("active");
-});
-
 const navlink=document.querySelectorAll(".nav-link")
+
+toggle.addEventListener("click", () => {
+  links.classList.toggle("active-nav");
+
+  if (links.classList.contains("active-nav")){
+    document.body.style.overflow="hidden"
+
+    navlink.forEach((eachlink)=>{
+      eachlink.addEventListener("click", (e)=>{
+        links.classList.remove("active-nav")
+        document.body.style.overflow="visible"
+      
+      })
+    })
+  
+  } else {
+    document.body.style.overflow="visible"
+  }
+ 
+});
 
 navlink.forEach((link)=>{
   link.addEventListener("click", linkaction)
