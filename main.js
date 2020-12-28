6a9d8b31311f18addd621e5c19a95c9e257f9dc6
P-oke <63716839+P-oke@users.noreply.github.com>
2020-12-28 12:25:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const toggle = document.getElementsByClassName("toggle-bar")[0];
-const links = document.getElementsByClassName("nav-elements")[0];
+const links = document.querySelector(".nav-elements")
 const navlink=document.querySelectorAll(".nav-link")
+const navcontent=document.querySelector(".nav-content")
+
 
 toggle.addEventListener("click", () => {
+    console.log("i");
   links.classList.toggle("active-nav");
+
 
   if (links.classList.contains("active-nav")){
     document.body.style.overflow="hidden"
@@ -32,4 +36,50 @@ function linkaction (){
     this.classList.add("active")
   })
 }
+
+window.addEventListener("scroll", ()=>{
+  
+  if(this.scrollY > 20 ){
+    console.log("hi");
+    navcontent.classList.add("sticky-nav")
+ } else {
+  navcontent.classList.remove("sticky-nav")
+ }
+
+})
+
+
+var typed = new Typed(".typing", {
+  strings: ["Backend developer", "freelancer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
+
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+
+sr.reveal('.home-content',{}); 
+sr.reveal('.home-img',{delay: 200}); 
+
+
+sr.reveal('.about-content',{}); 
+sr.reveal('.tech-stack',{delay: 400}); 
+
+
+
+sr.reveal('.portfolio-section',{});
+sr.reveal('.pf-image',{interval: 600}); 
+
+
+
+sr.reveal('.contact-section',{interval: 200}); 
+
+sr.reveal('.footer-section',{interval: 200}); 
 
